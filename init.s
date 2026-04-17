b1507bb6da2ea5d608706fb5d780842a3aad33b3
Marc A. Pelletier <mapelletier@datavalet.com>
2026-04-17 17:08:49 -0400

    Add "sample" init program

---
+++ init.s
@@ -0,0 +1,22 @@
+* "hard"coded bootstrap for the Nightmare vm
+
+_STACK		seg	0
+
+_DATA		seg	1
+		org	0
+greeting	db	"Hello, world!", 10, 0
+
+_BSS		seg	2
+		org	0
+var1		ds	4
+var2		ds	4
+
+_TEXT		seg	3
+
+		org	0
+_ini		lea	greeting,a0
+		mov	#3,d0
+		trap	#15
+		mov	#0,d1
+		mov	#4,d0
+		trap	#15
